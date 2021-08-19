49a57987a12282c5aee37d45d69f05e168a57379
Kordyukov <mrkordyukov@gmail.com>
2021-08-19 22:15:04 +0300

    complete

---
+++ src/com/alpha/work4/Task25.java
@@ -0,0 +1,7 @@
+package com.alpha.work4;
+
+public class Task25 {
+    public static void main(String[] args) {
+        new Runner().run();
+    }
+}
---
+++ src/com/alpha/work5/Task25.java
@@ -0,0 +1,7 @@
+package com.alpha.work5;
+
+public class Task25 {
+    public static void main(String[] args) {
+        new Runner().run();
+    }
+}
